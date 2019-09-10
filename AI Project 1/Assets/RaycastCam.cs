0ebde8e7541e5cf1f891aa69eed8077266239800
Shane-sama <48694586+ShaneLogtenberg@users.noreply.github.com>
2019-09-09 21:33:57 -0400

    Cone and rays

---
+++ AI Project 1/Assets/RaycastCam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastCam : MonoBehaviour
+{
+    public Camera mainCamera;
+    public GameObject objectHit;
+    RaycastHit hit;
+    Ray ray;
+
+    private void FixedUpdate()
+    {
+
+        ray = mainCamera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            objectHit = hit.transform.gameObject;
+            
+            Debug.Log("I'm looking at " + hit.transform.name);
+        }
+        else
+        {
+            objectHit = null;
+            Debug.Log("I'm looking at nothing!");
+        }
+    }
+}
